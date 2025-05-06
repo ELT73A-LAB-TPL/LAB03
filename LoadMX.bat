48794bfa1a9735de0345c7b0a920c4e0e835ac92
ruseleredu <ruseler@utfpr.edu.br>
2025-05-06 13:14:53 -0300

    Added some scripts

--- LoadMX.bat
+++ LoadMX.bat
@@ -2,22 +2,50 @@
 setlocal EnableDelayedExpansion
 
 :: Set Default Project Name
-set "DEFAULT_NAME=STM32F411CEUx_LAB02"
+set "DEFAULT_NAME=STM32F411CEUx"
+set "DEFAULT_SCRIPT=BaseScript.txt"
+set "DEFAULT_FLAG=N"
 
-:: Asks for Project Name
-set /p PJT_NAME="Enter a valid project name (or press Enter to use '%DEFAULT_NAME%'): "
-
-:: If input is empty, use the default name
-if "%PJT_NAME%"=="" (
+:: Check if arguments are passed, otherwise use defaults
+if "%~1"=="" (
     set "PJT_NAME=%DEFAULT_NAME%"
+) else (
+    set "PJT_NAME=%~1"
 )
 
-:: Display the stored project name
+if "%~2"=="" (
+    set "LOAD_SCRIPT=%DEFAULT_SCRIPT%"
+) else (
+    set "LOAD_SCRIPT=%~2"
+)
+
+if "%~3"=="" (
+    set "GC_FLAG=%DEFAULT_FLAG%"
+) else (
+    set "GC_FLAG=%~3"
+)
+
+
 echo Project name stored as: %PJT_NAME%
+echo Script file stored as: %LOAD_SCRIPT%
 
 :: Set the Documents path
 set "SCRIPT_PATH=%~dp0"
-set "SCRIPT_FILE=%SCRIPT_PATH%\LoadScript.txt"  :: Optional: Specify script file for -s, e.g., script.xml
+
+:: Define folder paths
+set "PJT_FOLDER=%SCRIPT_PATH%%PJT_NAME%"
+
+:: Check if project folder exists, create if not
+if not exist "%PJT_FOLDER%" (
+    echo Creating project folder...
+    mkdir "%PJT_FOLDER%"
+) else (
+    echo Project folder already exists.
+	pause
+	exit /b 1
+)
+
+set "SCRIPT_FILE=%PJT_FOLDER%\LoadScript.txt"  :: Optional: Specify script file for -s, e.g., script.xml
 
 if defined STM32CubeMX_PATH (
     if exist "%STM32CubeMX_PATH%" (
@@ -29,6 +57,20 @@ if defined STM32CubeMX_PATH (
     )
 ) else (
     echo STM32CubeMX_PATH is not defined.
+    pause
+    exit /b 1
+)
+
+if defined STM32CLT_PATH (
+    if exist "%STM32CLT_PATH%" (
+        echo STM32CLT_PATH exists and points to a valid directory: %STM32CLT_PATH%
+    ) else (
+        echo STM32CLT_PATH is defined but the directory does not exist: %STM32CLT_PATH%
+		pause
+		exit /b 1
+    )
+) else (
+    echo STM32CLT_PATH is not defined.
     pause
     exit /b 1
 )
@@ -58,29 +100,20 @@ if defined STM32CubeMX_PATH (
     set "STM32CUBEMX_PATH=%STM32CubeMX_PATH%\STM32CubeMX.exe"
 )
 
-set "DEFAULT_FLAG=N"
-set /p GC_FLAG="Generate Code? (Y/N, default is 'N'): "
-
-:: If input is empty, use default value
-if "%GC_FLAG%"=="" (
-    set "GC_FLAG=%DEFAULT_FLAG%"
-)
-
 :: Add project info
-copy /Y BaseScript.txt LoadScript.txt
-echo BaseScript.txt copied to LoadScript.txt successfully!
-echo project name %PJT_NAME% >> LoadScript.txt
-echo project toolchain "CMake" >> LoadScript.txt
-echo project path %SCRIPT_PATH% >> LoadScript.txt
-echo export script %SCRIPT_PATH%LoadedScript.txt >> LoadScript.txt
+copy /Y %LOAD_SCRIPT% %PJT_FOLDER%\LoadScript.txt
+echo project name %PJT_NAME% >> %PJT_FOLDER%\LoadScript.txt
+echo project toolchain "CMake" >> %PJT_FOLDER%\LoadScript.txt
+echo project path %SCRIPT_PATH% >> %PJT_FOLDER%\LoadScript.txt
+echo export script %PJT_FOLDER%\LoadedScript.txt >> %PJT_FOLDER%\LoadScript.txt
 
 :: Convert input to uppercase for consistency
 for %%A in (%GC_FLAG%) do set "GC_FLAG=%%A"
 
 if /i "%GC_FLAG%"=="Y" (
-    echo project generate >> LoadScript.txt
+    echo project generate %PJT_FOLDER% >> %PJT_FOLDER%\LoadScript.txt
 ) else if /i "%GC_FLAG%"=="N" (
-    echo #project generate >> LoadScript.txt
+    echo # project generate %PJT_FOLDER% >> %PJT_FOLDER%\LoadScript.txt
 ) else (
     echo Invalid entry. Please enter Y or N.
 )
@@ -103,5 +136,3 @@ if %ERRORLEVEL% neq 0 (
 echo STM32CubeMX executed successfully.
 pause
 endlocal
-
-
